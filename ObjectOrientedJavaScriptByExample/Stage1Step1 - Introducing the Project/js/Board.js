48dcc3505e5af9fa019dfa44efc43f273f276126
icld <76025734+icld@users.noreply.github.com>
2021-03-09 21:32:44 -0800

    foundation built

---
+++ ObjectOrientedJavaScriptByExample/Stage1Step1 - Introducing the Project/js/Board.js
@@ -0,0 +1,33 @@
+class Board {
+    constructor() {
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = this.createSpaces();
+    }
+
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {Array}     An array of space objects
+     */
+    createSpaces() {
+        // an array of columns where each column is an array of space objects
+        // we can also use this to access each column as a whole later when dropping tokens 
+        const spaces = [];
+
+        for (let x = 0; x < this.columns; x++) {
+            //empty array of columns
+            const column = [];
+            // iterate through the rows in columns and make new space objects 
+            // pass the space constructor method on them
+            // push new space on the column array
+            for (let y = 0; y < this.rows; y++) {
+                const space = new Space(x, y);
+                column.push(space);
+            }
+            // push the column back into the spaces array
+            spaces.push(column)
+        }
+
+        return spaces;
+    }
+}
